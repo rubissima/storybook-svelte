b1a9cba9812768ca853426d400a05dad3cbdfa28
rkane <rkane@squaretrade.com>
2022-02-25 16:32:31 -0800

    adding interactions and screen

--- src/components/TaskList.svelte
+++ src/components/TaskList.svelte
@@ -1,18 +1,18 @@
 <script>
-    import Task from './Task.svelte';
-    export let loading = false;
-    export let tasks = [];
+    import PureTaskList from './PureTaskList.svelte';
+    import { taskStore } from '../store';
+    function onPinTask(event) {
+      taskStore.pinTask(event.detail.id);
+    }
+    function onArchiveTask(event) {
+      taskStore.archiveTask(event.detail.id);
+    }
+  </script>
   
-    //👇 Reactive declarations (computed prop in other frameworks)
-    $: noTasks = tasks.length === 0;
-    $: emptyTasks = noTasks && !loading;
-</script>
-  {#if loading}
-    <div class="list-items">loading</div>
-  {/if}
-  {#if emptyTasks}
-    <div class="list-items">empty</div>
-  {/if}
-  {#each tasks as task}
-    <Task {task} on:onPinTask on:onArchiveTask />
-  {/each}
+  <div>
+    <PureTaskList
+      tasks={$taskStore}
+      on:onPinTask={onPinTask}
+      on:onArchiveTask={onArchiveTask}
+    />
+  </div>
